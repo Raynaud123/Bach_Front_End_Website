2b016d3c3be438161e63d9659a35ff48a38e5364
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-03-08 11:38:58 +0100

    Proberen nadoen Demo -> Data.js + vraagt topics op

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,57 @@
 import './App.css';
+import Data from './Data.js'
+import React from 'react';
+import axios from 'axios';
 
+/*
 function App() {
   return (
     <div className="App">
 
     </div>
   );
-}
+}*/
 
-export default App;
+const approvedTopics = axios.create({
+  baseURL: `http://localhost:8080/topics/approved`
+})
+
+export default class App extends React.Component{
+
+  state = {
+    hideData: false
+  }
+
+  knop = () => {
+    this.setState((state) => {
+      return{
+        hideData: !state.hideData
+      }
+    })
+  }
+  /*
+  toevoegen = async () => {
+    await students.post('/',{
+      name: "beke",
+    }).then(res => {
+      console.log(res);
+
+    }).catch(err => {
+      console.log("err" + err);
+    })
+
+    <button onClick={this.toevoegen}>Druk hier om persoon toe te voegen</button>
+
+  }*/
+
+  render()
+  {
+    return (
+        <div className="App">
+          <button onClick={this.knop}>Druk hier om de onderwerpen weg te laten of te tonen</button>
+          {this.state.hideData && <Data />}
+        </div>
+    );
+  }
+
+}
---
+++ website/src/Data.js
@@ -0,0 +1,41 @@
+import React from "react";
+import axios from "axios";
+
+const approvedTopics = axios.create({
+    baseURL: `http://localhost:8080/topics/approved`
+})
+
+export default class Data extends React.Component{
+
+    state = {
+        data : []
+        }
+
+        componentDidMount() {
+            approvedTopics.get('/').then(res => {
+                console.log(res);
+                this.setState({data :res.data})
+            })
+        }
+
+/*    get = ()=>{
+        students.get('/').then(res => {
+            console.log(res);
+            this.setState({data :res.data})
+        })
+    }*/
+
+//    constructor() {
+//        super();
+//        this.get();
+ //   }
+
+
+
+    render()
+    {
+        return (
+                <ul>{this.state.data.map(topic => <li key = {topic.topic_id}>{topic.topicName}</li>)}</ul>
+        );
+    };
+}

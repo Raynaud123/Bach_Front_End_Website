dc35d098de116eddfe20ac42479cfd23b36f6bcc
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-03-22 22:18:45 +0100

    update footer 2

--- website/src/Views/Footer.js
+++ website/src/Views/Footer.js
@@ -1,38 +1,26 @@
 import React from "react";
-import {FaRegCopyright} from "react-icons/all";
 import '../Styles/Footer.css';
-import KULeuvenLogo from '../assets/KULeuvenLogo.png'
 import {Facebook, Instagram, Twitter} from "react-bootstrap-icons";
 export default function Footer(){
     return(
         <div className={"footer"}>
-            <img className={"logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
-            <FaRegCopyright className={"icon"}/>
-            <Facebook className={"icon"}/>
-            <Twitter className={"icon"}/>
-            <Instagram className={"icon"}/>
+            <div className={"container"}>
+                <div className={"row"}>
+                    <div className="col-md-6 widget">
+                            <p>
+                                <Facebook />
+                                <Twitter />
+                                <Instagram />
+                            </p>
+                    </div>
+
+                    <div className="col-md-6 widget">
+                            <p className="text-right">
+                                Copyright &copy; 2022
+                            </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
-
-/*import React from "react";
-//import {FaBeer, FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter, FaYoutube} from 'react-icons/fa';
-import {
-    FooterBar, HR, BottomBar, MobileSocialList, Twitter, Facebook, Instagram
-} from "../Styles/FooterStyles";
-
-const Footer = () => {
-    return (
-        <FooterBar>
-            <HR />
-            <BottomBar>
-                <MobileSocialList>
-                    <Twitter />
-                    <Facebook />
-                    <Instagram />
-                </MobileSocialList>
-            </BottomBar>
-        </FooterBar>
-    );
-};
-export default Footer;*/

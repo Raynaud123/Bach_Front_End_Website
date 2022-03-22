2ce6da6e7c77581311a97789108730a2e34351d2
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-03-22 10:10:27 +0100

    onnuttig

--- website/src/index.js
+++ website/src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 
-/*ReactDOM.render(
+ReactDOM.render(
   <BrowserRouter>
       <Routes>
           <Route path="/" element={<App />}/>
@@ -16,7 +16,7 @@ const rootElement = document.getElementById("root");
       </Routes>
   </BrowserRouter>,
   rootElement
-);*/
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

dd820e0944f813a68e2259c61682311ce1ec04f6
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-03-22 10:02:34 +0100

    logos weg uit manifest.json

--- website/src/index.js
+++ website/src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 
-ReactDOM.render(
+/*ReactDOM.render(
   <BrowserRouter>
       <Routes>
           <Route path="/" element={<App />}/>
@@ -16,7 +16,7 @@ ReactDOM.render(
       </Routes>
   </BrowserRouter>,
   rootElement
-);
+);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

c9de5f3b7672abd5e4daf43724c8c573483324de
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-03-22 12:04:16 +0100

    update footer

--- website/src/Views/Footer.js
+++ website/src/Views/Footer.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { AiOutlineCopyright} from "react-icons/all";
+import {FaRegCopyright} from "react-icons/all";
 import '../Styles/Footer.css';
 import KULeuvenLogo from '../assets/KULeuvenLogo.png'
+import {Facebook, Instagram, Twitter} from "react-bootstrap-icons";
 export default function Footer(){
     return(
         <div className={"footer"}>
-            <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
-            <AiOutlineCopyright />
+            <img className={"logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+            <FaRegCopyright className={"icon"}/>
+            <Facebook className={"icon"}/>
+            <Twitter className={"icon"}/>
+            <Instagram className={"icon"}/>
         </div>
     )
 }

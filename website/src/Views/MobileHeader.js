6fdb2ef5ad6b90d1b6e37c2f4e372baaf93ec340
raynaud <raynaud.cornille@gmail.com>
2022-03-23 12:26:06 +0100

    Hamburger Menu

---
+++ website/src/Views/MobileHeader.js
@@ -0,0 +1,65 @@
+import React, {useState} from "react";
+import KULeuvenLogo from "../assets/KULeuvenLogo.png";
+import {Link} from "react-router-dom";
+import {GiHamburgerMenu, GoPerson, IoIosNotificationsOutline} from "react-icons/all";
+
+
+
+export default function MobileHeader(props){
+
+
+    const [navbarOpen, setNavbarOpen] = useState(false)
+
+    const role = props.roles;
+    const isLoggedIn = props.isLoggedIn;
+
+    const handleToggle = () => {
+        setNavbarOpen(prev => !prev)
+    }
+
+    if(!isLoggedIn){
+        return(
+            <div className={"header"}>
+                <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+            </div>
+        )}else if(role === "admin"){
+        return(
+            <div className={"header"}>
+                    <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+            </div>
+        )
+    }else if(role === "student"){
+        return(
+            <div className={"header hamburger"}>
+                    <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+                    <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
+                    {navbarOpen && <ul className={"menuNav"}>
+                        <div className={"hamburgerbuttons"}>
+                            <Link to="/Choice">Choice</Link>
+                            <Link to="/Topics">Topics</Link>
+                            <div className={"iconshamburger"}>
+                                <IoIosNotificationsOutline color='white' size={50}></IoIosNotificationsOutline>
+                                <GoPerson color='white' size={40}></GoPerson>
+                            </div>
+                        </div>
+                    </ul>}
+            </div>
+        )
+    }else if(role === "masterProef"){
+        return(
+            <div className={"header"}>
+                <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+
+            </div>
+        )
+    }else if(role === "bedrijf"){
+        return(
+            <div className={"header"}>
+                <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+
+            </div>
+        )
+    }
+
+
+}

9307818d869abc053c55306b0f549be2f4c7df20
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-03-27 00:23:43 +0100

    Begin filter topics

--- website/src/Views/topics/StudentTopic.js
+++ website/src/Views/topics/StudentTopic.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ApprovedTopics from "../topics/ApprovedTopics";
+import TopicFilter from "../topics/TopicFilter"
 import '../../Styles/Topics.css';
 
 export default function StudentTopic(){
@@ -9,9 +10,7 @@ export default function StudentTopic(){
                 <h1 className={"topicTitle titel"}> Masterthesis Topics </h1>
                 <h4 className={"ronde titel"}><b>First Round</b> </h4>
             </div>
-            <div className={"filter"}>
-                <a>Filter</a>
-            </div>
+            <TopicFilter />
             <ApprovedTopics />
         </div>
     )
---
+++ website/src/Views/topics/TopicFilter.js
@@ -0,0 +1,35 @@
+import React from "react";
+import axios from "axios";
+import {FiSearch} from "react-icons/all";
+import '../../Styles/Topics.css';
+
+
+const allTargetAudience = axios.create({
+    baseURL: `http://localhost:8080/targetaudience/all`
+})
+
+export default class TopicFilter extends React.Component{
+
+    state = {
+        data : []
+    }
+
+    componentDidMount() {
+        allTargetAudience.get('/').then(res => {
+            console.log(res);
+            this.setState({data :res.data})
+        })
+    }
+
+    render(){
+        return(
+            <div >
+                {/*{this.state.data.map(targetaudience =>*/}
+                {/*    <div key = {targetaudience.id}>*/}
+                {/*        {targetaudience.state.campus}*/}
+                {/*    </div>*/}
+                {/*)}*/}
+            </div>
+        )
+    };
+}

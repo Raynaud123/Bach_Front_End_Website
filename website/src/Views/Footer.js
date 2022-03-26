8fa46a9c7017aad91083cbd3ce58cdda51f97cd4
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-03-26 13:20:30 +0100

    Footer

--- website/src/Views/Footer.js
+++ website/src/Views/Footer.js
@@ -1,25 +1,38 @@
 import React from "react";
 import '../Styles/Footer.css';
-import {Facebook, Instagram, Twitter} from "react-bootstrap-icons";
+import KULeuvenLogo from "../assets/KULeuvenLogo.png";
+import {faYoutube,faFacebook,faTwitter,faInstagram,faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 export default function Footer(){
     return(
         <div className={"footer"}>
-            <div className={"container"}>
-                <div className={"row"}>
-                    <div className="col-md-6 widget">
-                            <p>
-                                <Facebook />
-                                <Twitter />
-                                <Instagram />
-                            </p>
-                    </div>
-
-                    <div className="col-md-6 widget">
-                            <p className="text-right">
-                                Copyright &copy; 2022
-                            </p>
-                    </div>
-                </div>
+            <div className={"footerbalk"}>
+                <img className={"logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+                <p>&copy; 2022</p>
+                <p><a href="https://admin.kuleuven.be/privacy/" target="_blank" className="footertekst">Disclaimer</a></p> {/*href="https://www.kuleuven.be/kuleuven/disclaimer.html"*/}
+                <p> <a href="https://admin.kuleuven.be/privacy/" target="_blank" className="footertekst">Privacy</a></p>  {/*href="https://admin.kuleuven.be/privacy/"*/}
+            </div>
+            <div className="social-container">
+                <a href="https://www.youtube.com/user/kuleuven/"
+                   className="youtube social">
+                    <FontAwesomeIcon icon={faYoutube} size="2x" />
+                </a>
+                <a href="https://www.facebook.com/KULeuven/"
+                   className="facebook social">
+                    <FontAwesomeIcon icon={faFacebook} size="2x" />
+                </a>
+                <a href="https://www.twitter.com/KU_Leuven" className="twitter social">
+                    <FontAwesomeIcon icon={faTwitter} size="2x" />
+                </a>
+                <a href="https://www.instagram.com/kuleuven"
+                   className="instagram social">
+                    <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+                <a href="https://www.linkedin.com/school/ku_leuven/"
+                   className="linkedin social">
+                    <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                </a>
             </div>
         </div>
     )

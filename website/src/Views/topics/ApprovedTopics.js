6a86d4cf8c3f43c036dd20c9d2291486215a87b7
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-03-28 19:29:41 +0200

    probeersel naam promotor

--- website/src/Views/topics/ApprovedTopics.js
+++ website/src/Views/topics/ApprovedTopics.js
@@ -8,23 +8,36 @@ const approvedTopics = axios.create({
     baseURL: `http://localhost:8080/topics/approved`
 })
 
+const promotors = axios.create({
+    baseURL: `http://localhost:8080/promotors/{id}`
+})
+
 export default class ApprovedTopics extends React.Component{
 
     state = {
-        data : []
+        apprTopics : [],
+        prom : [],
     }
 
     componentDidMount() {
         approvedTopics.get('/').then(res => {
             console.log(res);
-            this.setState({data :res.data})
+            this.setState({apprTopics :res.data})
         })
+        promotors.get('/').then(res => {
+            console.log(res);
+            this.setState({prom :res.data})
+        })
+        // axios.all([approvedTopics, promotors]).then(axios.spread((...res) => {
+        //     const topic = res[0]
+        //     const promotor = res[1]
+        // }))
     }
 
     render(){
         return(
             <div >
-                {this.state.data.map(topic =>
+                {this.state.apprTopics.map(topic =>
                     <div className={"topiccontainer"}>
                         <div key = {topic.id} className={"topicbox"}>
                             <div className={"topicmetheart"}>
@@ -50,6 +63,35 @@ export default class ApprovedTopics extends React.Component{
                     </div>
                 )}
             </div>
+            // <div >
+            //     {this.state.apprTopics.map(topic =>
+            //         <div className={"topiccontainer"}>
+            //             <div key = {topic.id} className={"topicbox"}>
+            //                 <div className={"topicmetheart"}>
+            //                     <div className={"topictitleinbox"}>
+            //                         {topic.topicName}
+            //                     </div>
+            //                     <PreferredTopic className={"buttonheart"} gevuld={false}></PreferredTopic>
+            //                 </div>
+            //                 <div className={"topicDescriptionbox contentintopicbow"}>
+            //                     {topic.description_topic}
+            //                 </div>
+            //                 <div className={"topicPromotorbox contentintopicbow"}>
+            //                     {this.state.prom.map(promotor => {
+            //                         {prom.promotor_id === topic.promotor_id} {promotor_id}
+            //                 }
+            //                 </div>
+            //                 <div className={"studentenmetinfo"}>
+            //                     <div className={"topicAantalStudentenbox contentintopicbow"}>
+            //                         <HiUsers className={"persoonicoontopic"}></HiUsers>
+            //                         aantal studenten: {topic.aantal_studenten}
+            //                     </div>
+            //                     <button className={"info_topic_button"}>Info</button>
+            //                 </div>
+            //             </div>
+            //         </div>
+            //     )}
+            // </div>
         )
     };
 }

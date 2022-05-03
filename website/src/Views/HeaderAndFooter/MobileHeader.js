593cc2edd9c728701e1c81f210f9e8fec255ec38
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-05-03 10:16:00 +0200

    Phasen ook in mobil header

--- website/src/Views/HeaderAndFooter/MobileHeader.js
+++ website/src/Views/HeaderAndFooter/MobileHeader.js
@@ -1,27 +1,62 @@
 import React, {useState} from "react";
 import KULeuvenLogo from "../../assets/KULeuvenLogo.png";
-import {Link} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import {GiHamburgerMenu, GoPerson, IoIosNotificationsOutline} from "react-icons/all";
+import useAxiosPrivate from "../../Hooks/useAxiosPrivate";
 
 
 export default function MobileHeader(props){
-
-
     const [navbarOpen, setNavbarOpen] = useState(false)
 
     const role = props.roles;
     const isLoggedIn = props.isLoggedIn;
+
+    const [PhaseFound, setPhaseFound] = useState(false);
+    const [PhaseId, setPhaseId] = useState(2);
+
+
+    const axiosPrivate = useAxiosPrivate();
+    const [errMsg] = useState('');
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    let isMounted = true;
+    const controller = new AbortController();
+    const getPhase = async () => {
+        try {
+            const response = await axiosPrivate({
+                method: "get",
+                url: "/phase/now",
+                signal: controller.signal
+            });
+            // console.log(response.data);
+            isMounted && setPhaseId(response.data.phase_id);
+        } catch (err) {
+            console.error(err);
+            navigate('/login', { state: { from: location }, replace: true });
+            console.log(errMsg);
+        }
+    }
 
     const handleToggle = () => {
         setNavbarOpen(prev => !prev)
     }
 
     if(!isLoggedIn || role === "NOTAPPROVED"){
+        if(!PhaseFound){
+            getPhase().then();
+            setPhaseFound(true);
+            return () => {
+                isMounted = false;
+                controller.abort();
+            }
+        }
         return(
             <div className={"header"}>
                 <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
             </div>
-        )}else if(role === "ADMIN"){
+        )}
+    else if(role === "ADMIN"){
         return(
             <div className={"header hamburger"}>
                 <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
@@ -39,9 +74,27 @@ export default function MobileHeader(props){
                 </ul>}
             </div>
         )
-    }else if(role === "STUDENT"){
-        return(
-            <div className={"header hamburger"}>
+    }
+    else if(role === "STUDENT"){
+        if (PhaseId === 1){
+            return(
+                <div className={"header hamburger"}>
+                    <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+                    <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
+                    {navbarOpen && <ul className={"menuNav"}>
+                        <div className={"hamburgerbuttons"}>
+                            <Link to="/submittopicstudent">Submit Topics</Link>
+                            <div className={"iconshamburger"}>
+                                <IoIosNotificationsOutline color='white' size={50}/>
+                                <GoPerson color='white' size={40}/>
+                            </div>
+                        </div>
+                    </ul>}
+                </div>)
+        }
+        if (PhaseId === 2 || PhaseId === 5){
+            return(
+                <div className={"header hamburger"}>
                     <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
                     <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
                     {navbarOpen && <ul className={"menuNav"}>
@@ -54,60 +107,163 @@ export default function MobileHeader(props){
                             </div>
                         </div>
                     </ul>}
-            </div>
-        )
-    }else if(role === "MASTER"){
-        return(
-            <div className={"header hamburger"}>
-                <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
-                <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
-                {navbarOpen && <ul className={"menuNav"}>
-                    <div className={"hamburgerbuttons"}>
-                        <Link to="/topic">Topics</Link>
-                        <Link to="/assign">Assign Topics</Link>
-                        <Link to="/approve">Approve Topics</Link>
-                        <div className={"iconshamburger"}>
-                            <IoIosNotificationsOutline color='white' size={50}/>
-                            <GoPerson color='white' size={40}/>
+                </div>
+            )
+        }
+        if (PhaseId === 3 || PhaseId === 4 || PhaseId === 6 || PhaseId === 7){
+            return(
+                <div className={"header hamburger"}>
+                    <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+                    <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
+                    {navbarOpen && <ul className={"menuNav"}>
+                        <div className={"hamburgerbuttons"}>
+                            <Link to="/viewchoice">Choice</Link>
+                            <div className={"iconshamburger"}>
+                                <IoIosNotificationsOutline color='white' size={50}/>
+                                <GoPerson color='white' size={40}/>
+                            </div>
                         </div>
-                    </div>
-                </ul>}
-            </div>
-        )
-    }else if(role === "COMPANY"){
-        return(
-            <div className={"header hamburger"}>
-                <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
-                <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
-                {navbarOpen && <ul className={"menuNav"}>
-                    <div className={"hamburgerbuttons"}>
-                        <Link to="/add">Add Topic</Link>
-                        <Link to="/topic">Topics</Link>
-                        <div className={"iconshamburger"}>
-                            <IoIosNotificationsOutline color='white' size={50}/>
-                            <GoPerson color='white' size={40}/>
+                    </ul>}
+                </div>
+            )
+        }
+        else
+            return null;
+    }
+    else if(role === "MASTER"){
+        if (PhaseId === 1 || PhaseId === 2 || PhaseId === 3 || PhaseId === 5 || PhaseId === 6){
+            return(
+                <div className={"header hamburger"}>
+                    <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+                    <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
+                    {navbarOpen && <ul className={"menuNav"}>
+                        <div className={"hamburgerbuttons"}>
+                            <Link to="/topic">Topics</Link>
+                            <Link to="/approve">Approve Topics</Link>
+                            <div className={"iconshamburger"}>
+                                <IoIosNotificationsOutline color='white' size={50}/>
+                                <GoPerson color='white' size={40}/>
+                            </div>
                         </div>
-                    </div>
-                </ul>}
-            </div>
-        )
-    }else if(role === "PROMOTOR"){
-        return(
-            <div className={"header hamburger"}>
-                <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
-                <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
-                {navbarOpen && <ul className={"menuNav"}>
-                    <div className={"hamburgerbuttons"}>
-                        <Link to="/topic">Topics</Link>
-                        <Link to="/add">Add Topic</Link>
-                        <Link to="/boost">boost Student</Link>
-                        <div className={"iconshamburger"}>
-                            <IoIosNotificationsOutline color='white' size={50}/>
-                            <GoPerson color='white' size={40}/>
+                    </ul>}
+                </div>)
+        }
+        if (PhaseId === 4 || PhaseId === 7){
+            return(
+                <div className={"header hamburger"}>
+                    <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+                    <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
+                    {navbarOpen && <ul className={"menuNav"}>
+                        <div className={"hamburgerbuttons"}>
+                            <Link to="/topic">Topics</Link>
+                            <Link to="/assign">Assign Topics</Link>
+                            <div className={"iconshamburger"}>
+                                <IoIosNotificationsOutline color='white' size={50}/>
+                                <GoPerson color='white' size={40}/>
+                            </div>
                         </div>
-                    </div>
-                </ul>}
-            </div>
-        )
+                    </ul>}
+                </div>
+            )
+        }
+        else
+            return null;
+
+    }
+    else if(role === "COMPANY"){
+        if (PhaseId === 2 || PhaseId === 3 || PhaseId === 4 || PhaseId === 5 || PhaseId === 6 || PhaseId === 7){
+            return(
+                <div className={"header hamburger"}>
+                    <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+                    <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
+                    {navbarOpen && <ul className={"menuNav"}>
+                        <div className={"hamburgerbuttons"}>
+                            <Link to="/topic">Topics</Link>
+                            <div className={"iconshamburger"}>
+                                <IoIosNotificationsOutline color='white' size={50}/>
+                                <GoPerson color='white' size={40}/>
+                            </div>
+                        </div>
+                    </ul>}
+                </div>
+            )
+        }
+        if (PhaseId === 1){
+            return(
+                <div className={"header hamburger"}>
+                    <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+                    <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
+                    {navbarOpen && <ul className={"menuNav"}>
+                        <div className={"hamburgerbuttons"}>
+                            <Link to="/add">Add Topic</Link>
+                            <Link to="/topic">Topics</Link>
+                            <div className={"iconshamburger"}>
+                                <IoIosNotificationsOutline color='white' size={50}/>
+                                <GoPerson color='white' size={40}/>
+                            </div>
+                        </div>
+                    </ul>}
+                </div>
+            )
+        }
+        else
+            return null;
+    }
+    else if(role === "PROMOTOR"){
+        if (PhaseId === 2 || PhaseId === 4 || PhaseId === 5 || PhaseId === 7){
+            return(
+                <div className={"header hamburger"}>
+                    <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+                    <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
+                    {navbarOpen && <ul className={"menuNav"}>
+                        <div className={"hamburgerbuttons"}>
+                            <Link to="/topic">Topics</Link>
+                            <div className={"iconshamburger"}>
+                                <IoIosNotificationsOutline color='white' size={50}/>
+                                <GoPerson color='white' size={40}/>
+                            </div>
+                        </div>
+                    </ul>}
+                </div>
+            )
+        }
+        if (PhaseId === 1){
+            return(
+                <div className={"header hamburger"}>
+                    <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+                    <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
+                    {navbarOpen && <ul className={"menuNav"}>
+                        <div className={"hamburgerbuttons"}>
+                            <Link to="/topic">Topics</Link>
+                            <Link to="/add">Add Topic</Link>
+                            <div className={"iconshamburger"}>
+                                <IoIosNotificationsOutline color='white' size={50}/>
+                                <GoPerson color='white' size={40}/>
+                            </div>
+                        </div>
+                    </ul>}
+                </div>
+            )
+        }
+        if (PhaseId === 3 || PhaseId === 6){
+            return(
+                <div className={"header hamburger"}>
+                    <img className={"Logo"} src={KULeuvenLogo} alt={"KU Leuven logo"}/>
+                    <GiHamburgerMenu size={50} className={"hamburgerbutton"} onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</GiHamburgerMenu>
+                    {navbarOpen && <ul className={"menuNav"}>
+                        <div className={"hamburgerbuttons"}>
+                            <Link to="/topic">Topics</Link>
+                            <Link to="/boost">boost Student</Link>
+                            <div className={"iconshamburger"}>
+                                <IoIosNotificationsOutline color='white' size={50}/>
+                                <GoPerson color='white' size={40}/>
+                            </div>
+                        </div>
+                    </ul>}
+                </div>
+            )
+        }
+        else
+            return null;
     }
 }
--- website/src/Views/HeaderAndFooter/NormalHeader.js
+++ website/src/Views/HeaderAndFooter/NormalHeader.js
@@ -10,7 +10,7 @@ export default function NormalHeader(props){
     const isLoggedIn = props.isLoggedIn;
 
     const [PhaseFound, setPhaseFound] = useState(false);
-    const [PhaseId, setPhaseId] = useState(1);
+    const [PhaseId, setPhaseId] = useState(2);
 
 
     const axiosPrivate = useAxiosPrivate();

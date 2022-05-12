112d67bf885933f59595eac42e15c26b17e2b49e
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-05-12 15:38:35 +0200

    top3 fix

--- website/src/Views/AppUsers/Student/TopicChoice_Student.js
+++ website/src/Views/AppUsers/Student/TopicChoice_Student.js
@@ -37,18 +37,23 @@ export default function TopicChoice_Student(props) {
             }
         }
         const getTop3 = async () => {
-            try {
-                const response = await axiosPrivate({
-                    method: "get",
-                    url: "/student/" + studentid + "/top3" ,
-                    signal: controller.signal
-                });
-                //console.log("Response top3: " + response.data);
-                isMounted && setTop3(response.data);
-            } catch (err) {
-                console.error(err);
-                navigate('/login', { state: { from: location }, replace: true });
-                console.log(errMsg + " fout bij top3: " + studentid + " top3: " + Top3);
+            if (Top3===[]) {
+                try {
+                    const response = await axiosPrivate({
+                        method: "get",
+                        url: "/student/" + studentid + "/top3" ,
+                        signal: controller.signal
+                    });
+                    //console.log("Response top3: " + response.data);
+                    isMounted && setTop3(response.data);
+                } catch (err) {
+                    console.error(err);
+                    navigate('/login', { state: { from: location }, replace: true });
+                    console.log(errMsg + " fout bij top3: " + studentid + " top3: " + Top3);
+                }
+            }
+            else {
+                console.log("Top3 groter dan 0");
             }
         }
         getPreferred();
@@ -67,8 +72,7 @@ export default function TopicChoice_Student(props) {
         console.log(Preferred);
         return (
             <div>
-                {
-                    Show?
+                {Show?
                         <Top3Pick_Student
                         studentid={studentid}
                         preferred={Preferred}
@@ -76,7 +80,7 @@ export default function TopicChoice_Student(props) {
                     :<div/>
                 }
                 <button onClick={() => setshow()} type={"button"}>{
-                    Show && Top3.length===0? <div>Don't show choice</div>:<div>Make choice Top 3 topics</div>
+                    Show? <div>Don't show choice</div>:<div>Make choice Top 3 topics</div>
                 }
                 </button>
             </div>
@@ -86,37 +90,37 @@ export default function TopicChoice_Student(props) {
     return(
         <div>
             <div>
-                {/*{*/}
-                {/*    Top3.length === 0? top3possible():<div>Already made top 3 choice</div>*/}
-                {/*}*/}
+                {
+                    Top3!==[]? top3possible():<div>Already made top 3 choice</div>
+                }
             </div>
             <div>
-            {/*    {Preferred.map((topic) => (*/}
-            {/*        <div className={"topiccontainer"}>*/}
-            {/*            <div key = {topic.id} className={"topicbox"}>*/}
-            {/*                <div className={"topicmetheart"}>*/}
-            {/*                    <div className={"topictitleinbox"}>*/}
-            {/*                        {topic.topicName}*/}
-            {/*                    </div>*/}
-            {/*                    <PreferredTopic className={"buttonheart"} topic_id={topic.topic_id} studentid={studentid}/>*/}
-            {/*                </div>*/}
-            {/*                <div className={"topicDescriptionbox contentintopicbow"}>*/}
-            {/*                    {topic.description_topic}*/}
-            {/*                </div>*/}
-            {/*                <div className={"studentenmetinfo"}>*/}
-            {/*                    <div className={"topicAantalStudentenbox contentintopicbow"}>*/}
-            {/*                        <HiUsers className={"persoonicoontopic"}/>*/}
-            {/*                        Number of students: {topic.aantal_studenten}*/}
-            {/*                    </div>*/}
-            {/*                    <button className={"info_topic_button"}>*/}
-            {/*                        <Link to={{*/}
-            {/*                            pathname: `/topics/info/${topic.topic_id}`,*/}
-            {/*                        }}*/}
-            {/*                        >Info</Link></button>*/}
-            {/*                </div>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    ))}*/}
+                {Preferred.map((topic) => (
+                    <div className={"topiccontainer"}>
+                        <div key = {topic.id} className={"topicbox"}>
+                            <div className={"topicmetheart"}>
+                                <div className={"topictitleinbox"}>
+                                    {topic.topicName}
+                                </div>
+                                <PreferredTopic className={"buttonheart"} topic_id={topic.topic_id} studentid={studentid}/>
+                            </div>
+                            <div className={"topicDescriptionbox contentintopicbow"}>
+                                {topic.description_topic}
+                            </div>
+                            <div className={"studentenmetinfo"}>
+                                <div className={"topicAantalStudentenbox contentintopicbow"}>
+                                    <HiUsers className={"persoonicoontopic"}/>
+                                    Number of students: {topic.aantal_studenten}
+                                </div>
+                                <button className={"info_topic_button"}>
+                                    <Link to={{
+                                        pathname: `/topics/info/${topic.topic_id}`,
+                                    }}
+                                    >Info</Link></button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )

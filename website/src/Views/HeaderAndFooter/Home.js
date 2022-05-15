2d132d23ec7dbd5b519da9582ef8f4318017dcaf
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-05-15 12:18:18 +0200

    Home fase doorgeven + master header assign promotor in fase 1 en 2

--- website/src/Views/HeaderAndFooter/Home.js
+++ website/src/Views/HeaderAndFooter/Home.js
@@ -10,7 +10,8 @@ import {useLocation, useNavigate} from "react-router-dom";
 
 export default function Home(props){
     const [Phase, setPhase] = useState([]);
-
+    const [BeginDate, setBeginDate] = useState("");
+    const [EndDate, setEndDate] = useState("");
 
     const axiosPrivate = useAxiosPrivate();
     const [errMsg] = useState('');
@@ -23,14 +24,39 @@ export default function Home(props){
         const controller = new AbortController();
 
         const getPhase = async () => {
+            function setDate(begin_deadline) {
+                function convertTZ(date, tzString) {
+                    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));
+                }
+                const convDate = convertTZ(begin_deadline,"Asia/Baku");
+                function padTo2Digits(num) {
+                    return num.toString().padStart(2, '0');
+                }
+                let formatDate = [
+                        convDate.getFullYear(),
+                        padTo2Digits(convDate.getMonth() + 1),
+                        padTo2Digits(convDate.getDate()),
+                    ].join('-') +
+                    ' ' +
+                    [
+                        padTo2Digits(convDate.getHours()),
+                        padTo2Digits(convDate.getMinutes()),
+                        padTo2Digits(convDate.getSeconds()),
+                    ].join(':')
+                console.log("formatDate " + formatDate);
+                return formatDate;
+            }
+
             try {
                 const response = await axiosPrivate({
                     method: "get",
                     url: "/phase/now",
                     signal: controller.signal
                 });
-                // console.log(response.data);
+                console.log(response.data);
                 isMounted && setPhase(response.data);
+                setBeginDate(setDate(response.data.begin_deadline));
+                setEndDate(setDate(response.data.begin_deadline));
             } catch (err) {
                 console.error(err);
                 navigate('/login', { state: { from: location }, replace: true });
@@ -46,15 +72,15 @@ export default function Home(props){
 
     switch(props.roles) {
         case "STUDENT":
-            return <Home_Student persoonid={props.persoonid} phase={Phase}/>
+            return <Home_Student persoonid={props.persoonid} phase={Phase} beginDate={BeginDate} endDate={EndDate}/>
         case "ADMIN":
-            return <Home_Admin persoonid={props.persoonid} phase={Phase}/>
+            return <Home_Admin persoonid={props.persoonid} phase={Phase} beginDate={BeginDate} endDate={EndDate}/>
         case "MASTER":
-            return <Home_Masterproef persoonid={props.persoonid} phase={Phase}/>
+            return <Home_Masterproef persoonid={props.persoonid} phase={Phase} beginDate={BeginDate} endDate={EndDate}/>
         case "COMPANY":
-            return <Home_Company persoonid={props.persoonid} phase={Phase}/>
+            return <Home_Company persoonid={props.persoonid} phase={Phase} beginDate={BeginDate} endDate={EndDate}/>
         case "PROMOTOR":
-            return <Home_Promotor persoonid={props.persoonid} phase={Phase}/>
+            return <Home_Promotor persoonid={props.persoonid} phase={Phase} beginDate={BeginDate} endDate={EndDate}/>
         case "NOTAPPROVED":
             return <Home_Standaard/>
         default:

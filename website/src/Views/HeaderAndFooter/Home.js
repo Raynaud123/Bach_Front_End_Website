2d952883847af704654352ddd0a182d906014fd9
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-05-15 15:41:29 +0200

    Home fix

--- website/src/Views/HeaderAndFooter/Home.js
+++ website/src/Views/HeaderAndFooter/Home.js
@@ -1,17 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import Home_Student from "../AppUsers/Student/Home_Student"
-import Home_Admin from "../AppUsers/Admin/Home_Admin"
-import Home_Masterproef from "../AppUsers/Masterproefcoordinator/Home_Masterproef"
-import Home_Company from "../AppUsers/Company/Home_Company"
-import Home_Promotor from "../AppUsers/Promotor/Home_Promotor"
 import Home_Standaard from "../Home_Standaard";
 import useAxiosPrivate from "../../Hooks/useAxiosPrivate";
 import {useLocation, useNavigate} from "react-router-dom";
+import "../../Styles/Home.css";
 
 export default function Home(props){
     const [Phase, setPhase] = useState([]);
     const [BeginDate, setBeginDate] = useState("");
     const [EndDate, setEndDate] = useState("");
+    const [FirstRound, setFirstRound] = useState(true);
 
     const axiosPrivate = useAxiosPrivate();
     const [errMsg] = useState('');
@@ -56,7 +53,8 @@ export default function Home(props){
                 console.log(response.data);
                 isMounted && setPhase(response.data);
                 setBeginDate(setDate(response.data.begin_deadline));
-                setEndDate(setDate(response.data.begin_deadline));
+                setEndDate(setDate(response.data.end_deadline));
+                setFirstRound(response.data.firstRound);
             } catch (err) {
                 console.error(err);
                 navigate('/login', { state: { from: location }, replace: true });
@@ -70,19 +68,77 @@ export default function Home(props){
         }
     }, [])
 
-    switch(props.roles) {
-        case "STUDENT":
-            return <Home_Student persoonid={props.persoonid} phase={Phase} beginDate={BeginDate} endDate={EndDate}/>
-        case "ADMIN":
-            return <Home_Admin persoonid={props.persoonid} phase={Phase} beginDate={BeginDate} endDate={EndDate}/>
-        case "MASTER":
-            return <Home_Masterproef persoonid={props.persoonid} phase={Phase} beginDate={BeginDate} endDate={EndDate}/>
-        case "COMPANY":
-            return <Home_Company persoonid={props.persoonid} phase={Phase} beginDate={BeginDate} endDate={EndDate}/>
-        case "PROMOTOR":
-            return <Home_Promotor persoonid={props.persoonid} phase={Phase} beginDate={BeginDate} endDate={EndDate}/>
-        case "NOTAPPROVED":
-            return <Home_Standaard/>
-        default:
+    function getTekst() {
+        switch(props.roles) {
+            case "STUDENT":
+                return (
+                    <div>
+                        As student, you can (depending on the current phase):
+                        <ul>
+                            <li>Submit Possible Thesis topics</li>
+                            <li>Like Thesis topics</li>
+                            <li>Choose Top3 Thesis topics from preferred topics</li>
+                            <li>View Top3</li>
+                        </ul>
+                    </div>
+                )
+            case "ADMIN":
+                return (
+                    <div>
+                        As admin you can have full control over everything. Take a look at "Maintenance".
+                    </div>
+                )
+            case "MASTER":
+                return (
+                    <div>
+                        As ThesisCoordinator, you can (depending on the current phase):
+                        <ul>
+                            <li>Submit Possible Thesis topics</li>
+                            <li>View added Thesis topics</li>
+                            <li>Approve Promotors/Companies/ResearchGroups</li>
+                            <li>Assign Promotors</li>
+                            <li>Assign Students to their Thesis topic</li>
+                        </ul>
+                    </div>
+                )
+            case "COMPANY":
+                return (
+                    <div>
+                        As Company or researchgroup, you can (depending on the current phase):
+                        <ul>
+                            <li>Submit Possible Thesis topics</li>
+                            <li>View added Thesis topics</li>
+                        </ul>
+                    </div>
+                )
+            case "PROMOTOR":
+                return (
+                    <div>
+                        As promotor, you can (depending on the current phase):
+                        <ul>
+                            <li>Submit Possible Thesis topics</li>
+                            <li>View added Thesis topics</li>
+                            <li>Boost Students for each Thesis topic</li>
+                        </ul>
+                    </div>
+                )
+            case "NOTAPPROVED":
+                return <Home_Standaard/>
+            default:
+        }
     }
+
+    return(
+        <div className={"standaard"}>
+            <div className={"titel"}>
+                <h1 className={"topicTitle titel"}>Home</h1>
+                <h4 className={"ronde titel"}> { FirstRound? <b>First Round</b> : <b>Second Round</b>}
+                    &emsp;&emsp; {Phase.phase_name} ({BeginDate} - {EndDate})</h4>
+            </div>
+            <br/>
+            {getTekst()}
+            <div className={"fill"}/>
+        </div>
+    )
+
 }
--- website/src/Views/Home_Standaard.js
+++ website/src/Views/Home_Standaard.js
@@ -2,6 +2,49 @@ import React from "react";
 
 export default function Home_Standaard(){
     return(
-        <p>Homepage</p>
+        <div className={"standaard"}>
+            <div className={"titel"}>
+                <h1 className={"topicTitle titel"}>Home</h1>
+                <div>
+                    This tool is for students/companies/researchgroups/promotors/ThesisCoordinators.
+                    <br/>
+                    <br/>
+                    <div>
+                        <b>Students</b> can (depending on the current phase):
+                        <ul>
+                            <li>Submit Possible Thesis topics</li>
+                            <li>Like Thesis topics</li>
+                            <li>Choose Top3 Thesis topics from preferred topics</li>
+                            <li>View Top3</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <b>Companies</b> or <b>researchgroups</b> can (depending on the current phase):
+                        <ul>
+                            <li>Submit Possible Thesis topics</li>
+                            <li>View added Thesis topics</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <b>Promotors</b> can (depending on the current phase):
+                        <ul>
+                            <li>Submit Possible Thesis topics</li>
+                            <li>View added Thesis topics</li>
+                            <li>Boost Students for each Thesis topic</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <b>ThesisCoordinators</b> can (depending on the current phase):
+                        <ul>
+                            <li>Submit Possible Thesis topics</li>
+                            <li>View added Thesis topics</li>
+                            <li>Approve Promotors/Companies/ResearchGroups</li>
+                            <li>Assign Promotors</li>
+                            <li>Assign Students to their Thesis topic</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }

63bc106cfd70bf559ed3b5125d8da23878a7b0ed
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-05-15 12:15:58 +0200

    Home

--- website/src/Views/AppUsers/Admin/Home_Admin.js
+++ website/src/Views/AppUsers/Admin/Home_Admin.js
@@ -1,17 +1,19 @@
-import React, {useEffect, useState} from "react";
-import useAxiosPrivate from "../../../Hooks/useAxiosPrivate";
-import {useLocation, useNavigate} from "react-router-dom";
+import React from "react";
+import "../../../Styles/Home.css";
 
 export default function Home_Admin(props) {
     const phase = props.phase;
     const firstRound = phase.firstRound;
 
     return(
-        <div className={"showTopics"}>
+        <div className={"standaard"}>
             <div className={"titel"}>
-                <h1 className={"topicTitle titel"}>Home Admin</h1>
+                <h1 className={"topicTitle titel"}>Home</h1>
                 <h4 className={"ronde titel"}> { firstRound? <b>First Round</b> : <b>Second Round</b>}
                     &emsp;&emsp; {phase.phase_name} ({phase.begin_deadline} - {phase.end_deadline})</h4>
+            </div>
+            <div>
+                As admin you can have full control over everything. Take a look at "Maintenance".
             </div>
         </div>
     )
